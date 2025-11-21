b3988cbdc6b05df645c9be3531254a7d79984211
Justin Le <lej42@uci.edu>
2025-11-20 22:57:24 -0800

    Imported QuoteSubmission into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import { useState, useEffect} from 'react';
 import QuoteList from './components/QuoteList'
+import QuoteSubmission from './components/QuoteSubmission'
 import FilterButtons from './components/FilterButtons'
 
 function App() {
 	const [quotes, setQuotes] = useState([])
 	const [filter, setFilter] = useState('all')
 
-	const handleFilterClick = (selected) => {
-		setFilter(selected);
-	}
+
+	const handleAddQuote = (newQuote) => {
+		setQuotes((prev) => [...prev, newQuote]);
+	};
 	
 	useEffect(() => {
 		let url = 'api/quotes'
@@ -38,13 +40,9 @@ function App() {
 
 			<h2>Submit a quote</h2>
 			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
-				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
-				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
-				<button type="submit">Submit</button>
-			</form>
+
+			<QuoteSubmission onAddQuote={handleAddQuote} />
+			
 			
 			<FilterButtons onFilterChange={setFilter} />
 
