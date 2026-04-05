ce0580047c8dfd70942c1e0c3fd8f2967b49d1de
Justin Le <lej42@uci.edu>
2026-04-04 22:08:13 -0700

    Reversed QuoteList

--- frontend/src/components/QuoteList.jsx
+++ frontend/src/components/QuoteList.jsx
@@ -3,7 +3,7 @@ import { timeAgo } from "../utils/timeAgo";
 export default function QuoteList({ quotes }) {
   return (
     <div className="quoteList">
-      {quotes.map((quote, index) => (
+      {[...quotes].reverse().map((quote, index) => (
         <div className="quoteItem" key={index}>
           <p className="quoteName">{quote.name}</p>
           <p className="quoteMessage">{quote.message}</p>
